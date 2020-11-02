7b024bf7f3bda7532b6de6aecd382db37ba22093
Niukkis <basanaama@gmail.com>
2020-11-02 05:17:54 +0200

    add default likes

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,16 +4,35 @@ const Blog = require('../models/blog')
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
     if (blogs) {
-      response.json(blogs)
+        response.json(blogs)
     } else {
-      response.status(404).end()
+        response.status(404).end()
     }
-  })
+})
   
-  blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
-    const savedBlog = await blog.save()
-    response.json(savedBlog)
-  })
+blogsRouter.post('/', async (request, response) => {
+    if (request.body.title && request.body.url) {
+        const blog = new Blog(request.body)
+        const savedBlog = await blog.save()
+        response.json(savedBlog)
+    } else {
+        response.status(400).end()
+    }
+})
 
-  module.exports = blogsRouter
+blogsRouter.get('/:id', async (request, response) => {
+    console.log(request.params.id)
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+        response.json(blog)
+    } else {
+        response.status(404).end()
+    }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+})
+
+module.exports = blogsRouter
