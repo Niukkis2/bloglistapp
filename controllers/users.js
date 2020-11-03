adb607eac5713707696b4e4d4f51d9c3e0dc3889
Niukkis <basanaama@gmail.com>
2020-11-03 07:43:56 +0200

    user creationand refactor

---
+++ controllers/users.js
@@ -0,0 +1,45 @@
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
+usersRouter.get('/', async (req, res) => {
+    const users = await User.find({})
+    if (users) {
+        res.json(users)
+    } else {
+        res.status(404).end()
+    }
+})
+
+usersRouter.get('/:id', async (req, res) => {
+    const user = await User.find(req.params.id)
+    if (user) {
+        res.json(user)
+    } else {
+        res.status(404).end()
+    }
+})
+
+usersRouter.post('/', async (req, res) => {
+    const checkCreds = async (uName, pW) => {
+        if (uName.length >= 3 && pW.length >= 3) {
+            const exists = await User.exists({ userName: uName })
+            return !exists
+        }
+        return false
+    }
+    if (await checkCreds(req.body.userName, req.body.passWord)) {
+        const saltRounds = 10
+        const user = new User({
+            userName: req.body.userName,
+            name: req.body.name,
+            passWord: await bcrypt.hash(req.body.passWord, saltRounds)
+        })
+        const savedUser = await user.save()
+        res.json(savedUser)
+    } else {
+        res.status(400).send('username and password must be atleast 3 characters long, username must be unique')
+    }
+})
+
+module.exports = usersRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,105 +3,122 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
-const initialBlogs = [
-    {
-        title: "Testblog 1",
-        author: "Testauthor 1",
-        url: "www.test1.com",
-        likes: 1
-    },
-    {
-        title: "Testblog 2",
-        author: "Testauthor 2",
-        url: "www.test2.com",
-        likes: 2
-    }
-]
-beforeEach(async () => {
-    await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlogs[0])
-    await blogObject.save()
-    blogObject = new Blog(initialBlogs[1])
-    await blogObject.save()
+const User = require('../models/user')
+const listHelper = require('../utils/list_helper')
+
+describe('blogs API tests', () => {
+    beforeEach(async () => {
+        await Blog.deleteMany({})
+        await Promise.all(listHelper.initialBlogs
+            .map(async blog => await new Blog(blog).save()))
+    })
+    describe('GET requests', () => {
+        test('blogs are returned as json', async () => {
+            await api
+                .get('/api/blogs')
+                .expect(200)
+                .expect('Content-Type', /application\/json/)
+        })
+        test('all blogs are returned', async () => {
+            const response = await api.get('/api/blogs')
+            expect(response.body).toHaveLength(listHelper.initialBlogs.length)
+        })
+        test('a specific blog is within the returned blogs', async () => {
+            const response = await api.get('/api/blogs')
+            const titles = response.body.map(r => r.title)
+            expect(titles).toContain('Testblog 2')
+        })
+        test('identifier property is named id', async () => {
+            const response = await api.get('/api/blogs/')
+            expect(response.body[0].id).toBeDefined()
+        })
+    })
+    
+    describe('POST requests', () => {
+        test('POST request creates new blog', async () => {
+            let newBlog = {
+                title: 'POST test',
+                author: 'POST test author',
+                url: 'www.POST.com',
+                likes: 2
+            }
+            const postRes = await api
+                .post('/api/blogs')
+                .send(newBlog)
+            newBlog.id = postRes.body.id
+            expect(postRes.body).toEqual(newBlog)
+            const res = await api.get('/api/blogs')
+            expect(res.body).toHaveLength(listHelper.initialBlogs.length + 1)
+        })
+        test('missing likes property defaults to 0', async () => {
+            const blogWithNoLikes = {
+                title: 'No likes',
+                author: 'No likes author',
+                url: 'www.nolikes.com'
+            }
+            const postRes = await api
+                .post('/api/blogs')
+                .send(blogWithNoLikes)
+            expect(postRes.body.likes).toBe(0)
+        })
+        test('title and url missing respond with bad request', async () => {
+            const blogMissingTitleAndUrl = {
+                author: 'missing',
+                likes: 5
+            }
+            const postRes = await api
+                .post('/api/blogs')
+                .send(blogMissingTitleAndUrl)
+            expect(postRes.status).toEqual(400)
+        })
+    })
+    
+    describe('DELETE requests', () => {
+        test('delete request removes blog', async () => {
+            const blogs = await api.get('/api/blogs')
+            const id = blogs.body[0].id
+            const delRes = await api
+                .delete(`/api/blogs/${id}`)
+            expect(delRes.status).toEqual(204)
+        })
+    })
+    
+    describe('PATCH requests', () => {
+        test('changing likes of blog', async () => {
+            const blogs = await api.get('/api/blogs')
+            const blog = {
+                title: blogs.body[0].title,
+                author: blogs.body[0].author,
+                url: blogs.body[0].url,
+                likes: blogs.body[0].likes + 1
+            }
+            const id = blogs.body[0].id
+            const patchRes = await api
+                .patch(`/api/blogs/${id}`)
+                .send(blog)
+            expect(patchRes.body.likes).toBe(blogs.body[0].likes + 1)
+        })
+    })
 })
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-})
-test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(initialBlogs.length)
-})
-test('a specific blog is within the returned blogs', async () => {
-    const response = await api.get('/api/blogs')
-    const titles = response.body.map(r => r.title)
-    expect(titles).toContain('Testblog 2')
-})
-test('identifier property is named id', async () => {
-    const response = await api.get('/api/blogs/')
-    expect(response.body[0].id).toBeDefined()
-})
-test('POST request creates new blog', async () => {
-    const newBlog = {
-        title: 'POST test',
-        author: 'POST test author',
-        url: 'www.POST.com',
-        likes: 2
-    }
-    const postRes = await api
-        .post('/api/blogs')
-        .send(newBlog)
-    console.log(postRes.body)
-    expect(postRes.body.title).toBe('POST test')
-    expect(postRes.body.author).toBe('POST test author')
-    expect(postRes.body.url).toBe('www.POST.com')
-    expect(postRes.body.likes).toBe(2)
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(initialBlogs.length + 1)
-})
-test('missing likes property defaults to 0', async () => {
-    const blogWithNoLikes = {
-        title: 'No likes',
-        author: 'No likes author',
-        url: 'www.nolikes.com'
-    }
-    const postRes = await api
-        .post('/api/blogs')
-        .send(blogWithNoLikes)
-    expect(postRes.body.likes).toBe(0)
-})
-test('title and url missing respond with bad request', async () => {
-    const blogMissingTitleAndUrl = {
-        author: 'missing',
-        likes: 5
-    }
-    const postRes = await api
-        .post('/api/blogs')
-        .send(blogMissingTitleAndUrl)
-    expect(postRes.status).toEqual(400)
-})
-test('delete request removes blog', async () => {
-    const blogs = await api.get('/api/blogs')
-    const id = blogs.body[0].id
-    const delRes = await api
-        .delete(`/api/blogs/${id}`)
-    expect(delRes.status).toEqual(204)
-})
-test('changing likes of blog', async () => {
-    const blogs = await api.get('/api/blogs')
-    const blog = {
-        title: blogs.body[0].title,
-        author: blogs.body[0].author,
-        url: blogs.body[0].url,
-        likes: blogs.body[0].likes + 1
-    }
-    const id = blogs.body[0].id
-    const patchRes = await api
-        .patch(`/api/blogs/${id}`)
-        .send(blog)
-    expect(patchRes.body.likes).toBe(blogs.body[0].likes + 1)
+
+describe('users API tests', () => {
+    beforeEach(async () => {
+        await Promise.all(listHelper.initialUsers
+            .map(async user => await new User(user).save()))
+    })
+    describe('POST requests', () => {
+        test('invalid user not created', async () => {
+            const invalidUser = {
+                userName: "no",
+                name: "invalid",
+                passWord: "pa"
+            }
+            const postRes = await api
+                .post('/api/users')
+                .send(invalidUser)
+            expect(postRes.status).toEqual(400)
+        })
+    })
 })
 afterAll(() => {
     mongoose.connection.close()
